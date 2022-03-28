a52ccc1cd4d56af588d5773e62562978f4b3ba0b
solo786 <abdul.karim08@gmail.com>
2022-03-28 21:59:10 +0000

    hourly changes

--- autorun.sh
+++ autorun.sh
@@ -7,6 +7,8 @@ python3 -m pip install requests
 cd $(dirname $0)/scripts/
 
 python3 youtube_m3ugrabber-makkah.py > ../youtube-makkah.m3u
+echo m3u makkah grabbed
+
 python3 youtube_m3ugrabber-medina.py > ../youtube-medina.m3u
 
-echo m3u grabbed
+echo m3u medina grabbed
